4773fa63bd3f403a271fa0979c4d56f3e12609a7
Administrator <Administrator@USER-20151122HL>
2017-03-09 22:28:51 +0800

    添加项目文件。

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Login = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(327, 285);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(128, 57);
+            this.button_Login.TabIndex = 0;
+            this.button_Login.Text = "登　陆";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(205, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 57);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开　始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(205, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 454);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Login);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Client/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Client
+{
+    public partial class MainForm : Form
+    {
+        HttpClient m_HttpClient = new HttpClient();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void init()
+        {
+            m_HttpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+            m_HttpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+            m_HttpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+            m_HttpClient.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("Chrome", "56.0.2924.76"));
+        }
+
+        private async void button_Login_Click(object sender, EventArgs e)
+        {
+            string vUrl = string.Format("http://learn.open.com.cn/Account/UnitLogin?t=0.08834811193444936&loginName={0}&passWord={1}&validateNum={2}&rememberMe=0",textBox1,textBox2,textBox3);
+            string body = await m_HttpClient.GetStringAsync(vUrl);
+            MessageBox.Show(body);
+        }
+
+        async Task<string> getWebValidatePic()
+        {
+            string body = await m_HttpClient.GetStringAsync(@"http://learn.open.com.cn/");
+            HtmlAgilityPack.HtmlDocument vHtmlDocument = new HtmlAgilityPack.HtmlDocument();
+            vHtmlDocument.LoadHtml(body);
+            
+            //m_HttpClient.DefaultRequestHeaders.
+            string vValidatePic = vHtmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"img_Validate\"]").Attributes["src"].Value;
+            vValidatePic = vValidatePic.Substring(0,vValidatePic.IndexOf('?'));
+            vValidatePic = string.Format("{0}?0.17864648687671529", vValidatePic);
+            return vValidatePic;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            string vValidatePic = await getWebValidatePic();
+            pictureBox1.Image = null;
+            pictureBox1.WaitOnLoad = false; //设置为异步加载图片  
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.LoadAsync(string.Format("http://learn.open.com.cn/{0}", vValidatePic));
+        }
+    }
+}
